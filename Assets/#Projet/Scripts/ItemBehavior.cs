ae8e685a961131daefc878a7e1969ff91e03e2b5
melanie <nahimana.melanie@gmail.com>
2021-08-31 16:37:31 +0200

    first commit

---
+++ Assets/#Projet/Scripts/ItemBehavior.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    public int id = -1;
+    public LevelManager manager;
+    public bool mouseOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && mouseOver) // click à gauche a le numéro ''0''
+        {
+
+            manager.RevealMaterial(id);
+            //Debug.Log($"Click: {id}");
+        }
+            
+    }
+
+    void OnMouseOver()
+    {
+        transform.localScale = new Vector3(1, 2, 1);
+        mouseOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        transform.localScale = Vector3.one;
+        mouseOver = false;
+    }
+}

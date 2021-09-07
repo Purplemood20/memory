823d477ffb467a6b1ef1c1ebbafec97ae928524c
melanie <nahimana.melanie@gmail.com>
2021-09-07 16:44:44 +0200

    animation end scene 2

---
+++ Assets/#Projet/Scripts/EndScore.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // using TMPro
+
+
+public class EndScore : MonoBehaviour
+{
+    public float seconds;
+    public Text text;   // ou public TextMeshProUGUI tmpText;
+    public string seconds_str;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seconds = PlayerPrefs.GetFloat("seconds",0f);
+        seconds_str = seconds.ToString("N2"); // "N2"->format:pour 2 chiffres après la virgule
+        text.text = "time:\n " + seconds_str;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

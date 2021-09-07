8886676dd4432bfb986262b152ff3ccefec954cc
melanie <nahimana.melanie@gmail.com>
2021-09-07 12:13:03 +0200

    2e animation menu + easy mode

---
+++ Assets/#Projet/Scripts/Menu_cubes_behavior.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_cubes_behavior : MonoBehaviour
+{
+    
+    
+    public bool mouseOver = false;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+
+        mouseOver = true;
+        animator.SetBool("MouseOver1", true);
+    }
+
+    void OnMouseExit()
+    {
+
+        mouseOver = false;
+        animator.SetBool("MouseOver1", false);
+    }
+
+
+
+}
